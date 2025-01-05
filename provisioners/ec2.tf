d241b0375e04d3672046522e4ce920389de53c25
chaitanyajonnadula <chaitanyajonnadula98@gmail.com>
2025-01-05 16:26:12 +0530

    file committed

--- provisioners/ec2.tf
+++ provisioners/ec2.tf
@@ -43,8 +43,17 @@ resource "aws_security_group" "terraform" {
 
     }
      ingress {
-        from_port = "22"
-        to_port = "22"
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+        
+    }
+
+     ingress {
+        from_port = 80
+        to_port = 80
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
         ipv6_cidr_blocks = ["::/0"]
