60bf5ad93cc33a2c081a555299585edf1abfec23
chaitanyajonnadula <chaitanyajonnadula98@gmail.com>
2025-08-03 12:19:13 +0530

    file committed

--- remote-state/ec2.tf
+++ remote-state/ec2.tf
@@ -27,6 +27,7 @@ resource "aws_instance" "terraform" {
     ami = "ami-09c813fb71547fc4f"
     instance_type = "t3.micro"
     vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
+    associate_public_ip_address = true
 
     tags = {
         Name = "terraform"
