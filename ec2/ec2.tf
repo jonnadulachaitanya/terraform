44a3e332f0b29af1411ff60008fc0a13a1045ff9
chaitanyajonnadula <chaitanyajonnadula98@gmail.com>
2025-08-03 13:34:29 +0530

    file committed

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -25,7 +25,7 @@ resource "aws_security_group" "allow_ssh_terraform" {
 
 resource "aws_instance" "terraform" {
     ami = "ami-09c813fb71547fc4f"
-    instance_type = "t3.micro"
+    instance_type = "t2.micro"
     vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
 
     tags = {
